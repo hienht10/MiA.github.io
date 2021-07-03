0936cddec929e782867a39b2dda4741c612cb867
hienht10 <86807548+hienht10@users.noreply.github.com>
2021-07-03 21:53:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -101,13 +101,13 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Why???'>",
-        background: '#fff url("img/123.jpg")',
-        backdrop: `
+         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy.gif")
               left bottom
               no-repeat
             `,
+        background: '#fff url("img/123.jpg")',
         confirmButtonColor: '#3085d6',
         confirmButtonColor: '#fe8a71',
         confirmButtonText: 'Send to me <3'
